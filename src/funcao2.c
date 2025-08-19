3325e7e113d62cf6f83f760eaae560a56db113bf
Eduardo Mode <modeoftalmo@uol.com.br>
2025-08-19 14:54:00 +0000

    Exemplos de utilização de função em c

---
+++ src/funcao2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../lib/funcao2.h"
+
+
+int main() {
+    mensagem();
+    printf("\n");
+    separador();
+    printf("\n");
+    printf("%d\n",precoTotal(5,10));
+    return 0;
+}
+
