dbd6c800825a8415857176564befd16fdf3077be
Eduardo Mode <modeoftalmo@uol.com.br>
2025-08-19 14:57:51 +0000

    Pasta de biblioteca de coamandos

---
+++ lib/tarefa1extensao.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *criar(char *nomeArquivo, char *conteudo){ 
+    FILE *arquivo;
+
+    arquivo = fopen(nomeArquivo, "a");
+    
+    //fprintf(arquivo, conteudo);
+    fputs(conteudo, arquivo);
+
+    fclose(arquivo);
+
+    return "Arquivo criado com sucesso!";
+
+
+}
+
+float jurosSimples(float preco, float taxa){
+
+    return preco * (taxa / 100);
+}
